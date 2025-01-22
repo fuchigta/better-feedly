e3a4ce5145a84fb2373ee9ec6857f2eea5e54a9b
fuchigta <fuchigta@alpha.co.jp>
2025-01-23 01:08:01 +0900

    最初のコミット

---
+++ src/popup.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react"
+
+function getFeedURL() {
+  const rssLink: any = document.querySelector(
+    'link[type="application/rss+xml"]'
+  )
+  if (rssLink) {
+    return rssLink.href
+  }
+
+  return null
+}
+
+async function getCurrentTabFeedURL() {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  })
+
+  const [{ result }] = await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: getFeedURL
+  })
+
+  return result
+}
+
+function IndexPopup() {
+  useEffect(() => {
+    getCurrentTabFeedURL().then((res) => {
+      if (res) {
+        window.open(
+          `https://feedly.com/i/subscription/feed${encodeURIComponent("/" + res)}`
+        )
+      }
+    })
+  }, [])
+  return null
+}
+
+export default IndexPopup
